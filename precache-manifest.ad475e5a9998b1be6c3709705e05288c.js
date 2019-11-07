96ca27fe8f57297e7161c6662d3f0097a02562f4
Gatlin Carrier <gatlin.carrier@gmail.com>
2019-11-07 14:10:24 -0500

    Updates

---
+++ precache-manifest.ad475e5a9998b1be6c3709705e05288c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf95ef073eb5091403231ac6d8db63e",
+    "url": "/pokemon-react/index.html"
+  },
+  {
+    "revision": "02f1b3e5560e282da694",
+    "url": "/pokemon-react/static/css/main.7b41c070.chunk.css"
+  },
+  {
+    "revision": "23a8bbc4169bc7af7e44",
+    "url": "/pokemon-react/static/js/2.e32c25a0.chunk.js"
+  },
+  {
+    "revision": "02f1b3e5560e282da694",
+    "url": "/pokemon-react/static/js/main.475c69ca.chunk.js"
+  },
+  {
+    "revision": "8741c8fc85656c86a6b4",
+    "url": "/pokemon-react/static/js/runtime-main.e4b7ddb4.js"
+  }
+]);
